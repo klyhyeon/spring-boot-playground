3bef8f2f11efbf76f383fb0e841025951acd9985
baekho <klyhyeon@gmail.com>
2024-07-04 20:47:48 +0900

    feat initial commit

---
+++ src/test/kotlin/com/spring_boot/lab/LabApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spring_boot.lab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
