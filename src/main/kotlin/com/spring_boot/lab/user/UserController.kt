8b8ca2d6bb35564215f4984f1d8eea144284b520
baekho <klyhyeon@gmail.com>
2024-07-04 22:33:32 +0900

    fix refactor controller endpoint, fun naming

--- src/main/kotlin/com/spring_boot/lab/user/UserController.kt
+++ src/main/kotlin/com/spring_boot/lab/user/UserController.kt
@@ -3,6 +3,7 @@ package com.spring_boot.lab.user
 import com.spring_boot.lab.team.Team
 import com.spring_boot.lab.team.TeamRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import kotlin.random.Random
@@ -15,23 +16,7 @@ class UserController(
 ) {
 
 
-    @GetMapping
-    fun helloLab(): String {
-        val newUser = userRepository.save(
-            User(
-                name = "test",
-            )
-        )
-        println("newUser: $newUser")
-        val result = userRepository.findAll()
-        for (ele in result) {
-            var i = 0
-            println("result ${++i}: $ele")
-        }
-        return "Hello Lab"
-    }
-
-    @GetMapping("/user/team")
+    @PostMapping("/user-team")
     fun newUserNewTeam(): UserDto {
         val newTeam = teamRepository.save(
             Team(
