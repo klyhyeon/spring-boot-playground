beb126ef3020916a71c94bd07fbb92c49e03249b
baekho <klyhyeon@gmail.com>
2024-07-04 22:35:21 +0900

    comment add JPQL projection

--- src/main/kotlin/com/spring_boot/lab/user/UserRepository.kt
+++ src/main/kotlin/com/spring_boot/lab/user/UserRepository.kt
@@ -11,6 +11,7 @@ interface UserRepository : JpaRepository<User, Long> {
     fun findAllProjected(): List<UserDto>
 
 
+    // JPQL 프로젝션을 사용할 때 DTO에 객체 필드가 있다면 엔티티로 받은 다음 DTO 클래스에서 매핑해줘야 함
     @Query(
         "SELECT new com.spring_boot.lab.user.UserDto(u.id, u.name, u.createdAt, u.updatedAt, t) " +
                 "FROM User u JOIN u.team t WHERE u.id = ?1"
